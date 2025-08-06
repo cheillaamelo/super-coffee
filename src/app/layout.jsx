34fc7fc5975b619feda9717f33f6e26cfe1cfa20
cheillaamelo <cheillaa@hotmail.com>
2025-08-06 09:53:39 -0300

    add metadata image

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,8 +13,12 @@ const inria_serif = Inria_Serif({
 
 export const metadata = {
   title: "Super Coffee",
-  description: "",
-  image: "/images/pictures/about.png",
+  description: "Seja bem-vindo ao Super Coffee.",
+  openGraph: {
+    title: "Super Coffee",
+    description: "Seja bem-vindo ao Super Coffee.",
+    images: "/images/pictures/about.png",
+  },
 };
 
 export default function RootLayout({ children }) {
